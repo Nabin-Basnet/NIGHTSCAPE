4538e4adaf9900909ea1e82fbfb41ae393073407
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-19 12:35:31 +0545

    database completed

--- BACKEND/EcommerceBackend/EcommerceBackend/settings.py
+++ BACKEND/EcommerceBackend/EcommerceBackend/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'EcommerceBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_USIbvZ03xQlX',
+        'HOST': 'ep-shiny-bonus-a489ux4b-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',  # default PostgreSQL port
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
