a469f2fff27f023036e771753e00bdf8c4011d93
nabin1200 <nabinbasnet1200@gmail.com>
2025-06-01 17:28:45 +0545

    created some api(product,category,cart,order,orderitem)

--- BACKEND/EcommerceBackend/ecommerce/urls.py
+++ BACKEND/EcommerceBackend/ecommerce/urls.py
@@ -1,6 +1,15 @@
 from django.urls import include,path
 from ecommerce.views import landing
+from .views import ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet, OrderItemViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'carts', CartViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns=[
     path("",landing),
+    path('api/', include(router.urls)),
 ]
--- BACKEND/EcommerceBackend/ecommerce/views.py
+++ BACKEND/EcommerceBackend/ecommerce/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse,request
+from django.http import HttpResponse, request
+from rest_framework import viewsets
+from .models import (User,Product,Category,Brand,ProductImage,
+                    Address,Order,OrderItem,Cart,Wishlist,
+                    Payment,Review,Return,FeaturedProduct)
+from .serializers import (UserSerializer,CategorySerializer,BrandSerializer,
+                          ProductSerializer,AddressSerializer,OrderItemSerializer,
+                          OrderSerializer,CartSerializer,WishlistSerializer,
+                          PaymentSerializer,ReviewSerializer,ReturnSerializer,
+                          FeaturedProductSerializer)
+
 
 # Create your views here.
 def landing(request):
     return HttpResponse("hello buddy")
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
